3f608b8a7914a43e6c26b3c890df9043dd65ad65
6Sravani <marojusravani679@gmail.com>
2025-08-29 15:51:37 +0530

    chore(backend): initialize Spring Boot application with database connection

---
+++ Backend/store/src/test/java/com/uni_store/store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uni_store.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
