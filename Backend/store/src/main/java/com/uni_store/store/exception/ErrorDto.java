c809be6dcf234fd62d403035a2aef27e9fb941b3
6Sravani <marojusravani679@gmail.com>
2025-10-17 14:57:52 +0530

    fix(dto): use @JsonFormat to format timestamp in ErrorDto

--- Backend/store/src/main/java/com/uni_store/store/exception/ErrorDto.java
+++ Backend/store/src/main/java/com/uni_store/store/exception/ErrorDto.java
@@ -1,8 +1,11 @@
 package com.uni_store.store.exception;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.Instant;
 
 public record ErrorDto (
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Kolkata")
         Instant timestamp,
         int status,
         String error,
